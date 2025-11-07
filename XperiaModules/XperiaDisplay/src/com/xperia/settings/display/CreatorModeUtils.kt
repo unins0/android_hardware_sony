unins0/hardware_sony

XperiaModules: Implement Modular Framework
* Credits go to @lolipuru for the modular Xperia Modules framework (And for logic that hides the entries if the APKs aren't present)

* There is only one main package which is Xperia Settings of course, after that we have the 3 main modules which are: Display, Audio and Charger.

* And lastly the 3 submodules: Switcher, DIRAC and Dolby

* Refractored the main package from Xperia Parts > Xperia Settings

* Removed LineageOS Dependencies (Since they're not needed now)

* Xperia Dolby isn't being built as of for now since it's currently broken on Android 14

* Translated 360 Upmix into all the languages Xperia Settings supports and moved it from extra to hardware

Signed-off-by: saku-bruh <notwolfyy@gmail.com>
Co-Authored-By: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>

XperiaModules: Add EUICC module

XperiaModules: Fix Modules
* Fixed DIRAC, Audio & Charger menus crashing

* Added SEPolicy rules for DIRAC (and moved charger SEPolicy rules to the XperiaCharger module folder, same goes for DIRAC)

Signed-off-by: saku-bruh <notwolfyy@gmail.com>

XperiaSwitcher: Use TOGGLEABLE_TILE

Signed-off-by: saku-bruh <notwolfyy@gmail.com>

XperiaDirac: Use TOGGLEABLE_TILE

Change-Id: I192065cc4a8ba6993974036b60f803123f5a87cc

XperiaDolby: Use TOGGLEABLE_TILE

Change-Id: Idfe45cdfe280751235f99784c6666e9c02d3a8f2

hardware: XperiaModules: Import Common SEPolicy Rules

* And adapt them

From:
https://github.com/TheParasiteProject-Devices/packages_apps_XiaomiParts

Change-Id: Ica89b7b93decbaa585b08b1d642373652e90321d

hardware: XperiaDirac: Use MiSound FX UUID

Change-Id: I807aa153efefa024bbb3da1bef09e45b652646fa

XperiaDirac: Add MiSound Scenes

Original commit
https://github.com/lostark13/device_xiaomi_surya/commit/9146f08af4fd913b66e052d8c11c85d619eb26a4

[someone5678]
[1] set default value as disabled
[2] change strings
[3] remove icon
[4] adapt to current dirac code
[5] only enable when Hi-Fi mode is disabled

[Saku]:
*

XperiaModules/XperiaDisplay/src/com/xperia/settings/display/CreatorModeUtils.kt
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2023 XperiaLabs Project
+ * Copyright (C) 2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.xperia.settings.display
+
+import android.app.Activity
+import android.content.Context
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import androidx.core.content.ContextCompat
+import android.hardware.display.ColorDisplayManager
+
+import vendor.semc.hardware.display.V2_0.IDisplay
+import vendor.semc.hardware.display.V2_0.IDisplayCallback
+import vendor.semc.hardware.display.V2_0.PccMatrix
+
+class CreatorModeUtils(private val context: Context) : IDisplayCallback.Stub() {
+    private val colorDisplayManager: ColorDisplayManager =
+            context.getSystemService(ColorDisplayManager::class.java)
+                    ?: throw Exception("Display manager is NULL")
+    private val semcDisplayService: IDisplay =
+            IDisplay.getService() ?: throw Exception("SEMC Display HIDL not found")
+
+    val isEnabled: Boolean
+        get() = Settings.Secure.getInt(context.contentResolver, CREATOR_MODE_ENABLE, 0) != 0
+
+    fun setMode(enabled: Boolean) {
+        semcDisplayService.set_sspp_color_mode(if (enabled) 0 else 1)
+        colorDisplayManager.setColorMode(if (enabled) 0 else 3)
+        semcDisplayService.set_color_mode(if (enabled) 0 else 1)
+
+        Settings.Secure.putInt(context.contentResolver, CREATOR_MODE_ENABLE, if (enabled) 1 else 0)
+    }
+
+    fun initialize() {
+        Log.e(TAG, "Creator Mode controller setup")
+
+        if (!isEnabled) {
+            semcDisplayService.set_sspp_color_mode(1)
+            colorDisplayManager.setColorMode(3)
+            semcDisplayService.set_color_mode(1)
+        }
+
+        semcDisplayService.registerCallback(this)
+        semcDisplayService.setup()
+    }
+
+    override fun onWhiteBalanceMatrixChanged(matrix: PccMatrix) {
+        val colorMatrix: ColorMatrix = ColorMatrix().apply {
+            set(floatArrayOf(
+                    matrix.red, matrix.green, matrix.blue, 0f, 0f,
+                    matrix.red, matrix.green, matrix.blue, 0f, 0f,
+                    matrix.red, matrix.green, matrix.blue, 0f, 0f,
+                    0f, 0f, 0f, 1f, 0f
+            ))
+        }
+
+        val filter = ColorMatrixColorFilter(colorMatrix)
+
+        val views = (context as Activity).window.decorView
+        views.post { views.background.colorFilter = filter }
+        Log.i(TAG, "New white balance: ${matrix.red}, ${matrix.green}, ${matrix.blue}")
+    }
+
+    companion object {
+        private const val TAG = "CreatorModeUtils"
+        private const val CREATOR_MODE_ENABLE = "cm_enable"
+    }
+}

unins0/android_hardware_sony

XperiaModules/XperiaDisplay/src/com/xperia/settings/display/CreatorModeUtils.kt
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2023 XperiaLabs Project
+ * Copyright (C) 2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.xperia.settings.display
+
+import android.app.Activity
+import android.content.Context
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import androidx.core.content.ContextCompat
+import android.hardware.display.ColorDisplayManager
+
+import vendor.semc.hardware.display.V2_0.IDisplay
+import vendor.semc.hardware.display.V2_0.IDisplayCallback
+import vendor.semc.hardware.display.V2_0.PccMatrix
+
+class CreatorModeUtils(private val context: Context) : IDisplayCallback.Stub() {
+    private val colorDisplayManager: ColorDisplayManager =
+            context.getSystemService(ColorDisplayManager::class.java)
+                    ?: throw Exception("Display manager is NULL")
+    private val semcDisplayService: IDisplay by lazy {
+        val service = IDisplay.getService() ?: throw Exception("SEMC Display HIDL not found")
+
+        // Register itself as callback for HIDL
+        service.registerCallback(this)
+
+        service.setup()
+        service
+    }
+
+    val isEnabled: Boolean
+        get() = Settings.Secure.getInt(context.contentResolver, CREATOR_MODE_ENABLE, 0) != 0
+
+    fun setMode(enabled: Boolean) {
+        semcDisplayService.set_sspp_color_mode(if (enabled) 0 else 1)
+        colorDisplayManager.setColorMode(if (enabled) 0 else 3)
+        semcDisplayService.set_color_mode(if (enabled) 0 else 1)
+
+        Settings.Secure.putInt(context.contentResolver, CREATOR_MODE_ENABLE, if (enabled) 1 else 0)
+    }
+
+    fun initialize() {
+        Log.e(TAG, "Creator Mode controller setup")
+
+        if (!isEnabled) {
+            semcDisplayService.set_sspp_color_mode(1)
+            colorDisplayManager.setColorMode(3)
+            semcDisplayService.set_color_mode(1)
+        }
+
+    }
+
+    override fun onWhiteBalanceMatrixChanged(matrix: PccMatrix) {
+        val colorMatrix: ColorMatrix = ColorMatrix().apply {
+            set(floatArrayOf(
+                    matrix.red, matrix.green, matrix.blue, 0f, 0f,
+                    matrix.red, matrix.green, matrix.blue, 0f, 0f,
+                    matrix.red, matrix.green, matrix.blue, 0f, 0f,
+                    0f, 0f, 0f, 1f, 0f
+            ))
+        }
+
+        val filter = ColorMatrixColorFilter(colorMatrix)
+
+        val views = (context as Activity).window.decorView
+        views.post { views.background.colorFilter = filter }
+        Log.i(TAG, "New white balance: ${matrix.red}, ${matrix.green}, ${matrix.blue}")
+    }
+
+    companion object {
+        private const val TAG = "CreatorModeUtils"
+        private const val CREATOR_MODE_ENABLE = "cm_enable"
+    }
+}